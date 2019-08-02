9c1de0fcdbe047a6befb4de3107d3a8816b9523f
unknownsadness <694678915@qq.com>
2019-08-02 23:43:22 +0800

    单例模式的实现

---
+++ design-pattern/src/main/java/bird/free/singleton/SingletonV2.java
@@ -0,0 +1,24 @@
+package bird.free.singleton;
+
+/**
+ * @author freebird
+ */
+public class SingletonV2 {
+
+    private static volatile SingletonV2 instance = null;
+
+    private SingletonV2(){}
+
+    public static SingletonV2 getInstance(){
+        if(instance == null){
+            synchronized (SingletonV2.class){
+                if(instance == null){
+                    synchronized (SingletonV2.class){
+                        instance = new SingletonV2();
+                    }
+                }
+            }
+        }
+        return instance;
+    }
+}
